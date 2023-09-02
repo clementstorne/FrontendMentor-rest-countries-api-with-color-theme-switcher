acd6299bf5d25b45b9be8bc3679f812b1971c62b
Clément Storne <clementstorne@gmail.com>
2023-09-02 16:04:57 +0200

    desktop header done

--- src/components/DarkModeButton.tsx
+++ src/components/DarkModeButton.tsx
@@ -1,38 +1,47 @@
-import { useEffect } from "react";
-import { IoMoonOutline } from "react-icons/io5";
+import { useEffect, useState } from "react";
+import { IoMoonOutline, IoMoonSharp } from "react-icons/io5";
 
-const switchDarkMode = (e: React.MouseEvent<HTMLButtonElement>): void => {
-  e.preventDefault;
-  const isDarkModeEnabled: string | null = localStorage.getItem("darkMode");
+const DarkModeButton = (): JSX.Element => {
+  const [darkMode, setDarkMode] = useState(localStorage.getItem("darkMode"));
 
-  if (isDarkModeEnabled) {
-    if (isDarkModeEnabled === "true") {
-      localStorage.setItem("darkMode", "false");
-      document.documentElement.classList.remove("dark");
+  const switchDarkMode = (e: React.MouseEvent<HTMLButtonElement>): void => {
+    e.preventDefault;
+    const isDarkModeEnabled: string | null = localStorage.getItem("darkMode");
+
+    if (isDarkModeEnabled) {
+      if (isDarkModeEnabled === "true") {
+        localStorage.setItem("darkMode", "false");
+        document.documentElement.classList.remove("dark");
+        setDarkMode("false");
+      } else {
+        localStorage.setItem("darkMode", "true");
+        document.documentElement.classList.add("dark");
+        setDarkMode("true");
+      }
     } else {
       localStorage.setItem("darkMode", "true");
       document.documentElement.classList.add("dark");
+      setDarkMode("true");
     }
-  } else {
-    localStorage.setItem("darkMode", "true");
-    document.documentElement.classList.add("dark");
-  }
-};
+  };
 
-const DarkModeButton = (): JSX.Element => {
   useEffect(() => {
-    if (localStorage.getItem("darkMode") === "true") {
+    if (darkMode === "true") {
       document.documentElement.classList.add("dark");
     }
-  });
+  }, [darkMode]);
 
   return (
     <button
       className="flex flex-row flex-nowrap items-center"
       onClick={switchDarkMode}
     >
-      <IoMoonOutline size={22} className="pr-2" />
-      <span className="text-xs font-semibold">Dark Mode</span>
+      {darkMode === "true" ? (
+        <IoMoonSharp className="h-6 w-6 md:h-7 md:w-7 pr-2" />
+      ) : (
+        <IoMoonOutline className="h-6 w-6 md:h-7 md:w-7 pr-2" />
+      )}
+      <span className="text-xs md:text-base font-semibold">Dark Mode</span>
     </button>
   );
 };
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,8 +2,10 @@ import DarkModeButton from "./DarkModeButton";
 
 const Header = (): JSX.Element => {
   return (
-    <header className="bg-lightElement dark:bg-darkElement drop-shadow-[0_1px_4px_rgba(0,0,0,0.1)] h-20 w-full px-4 flex flex-row flex-nowrap justify-between items-center">
-      <h1 className="text-sm font-extrabold">Where in the world?</h1>
+    <header className="bg-lightElement dark:bg-darkElement drop-shadow-[0_1px_4px_rgba(0,0,0,0.1)] h-20 w-full px-4 md:px-20 flex flex-row flex-nowrap justify-between items-center">
+      <h1 className="text-sm font-extrabold md:text-2xl">
+        Where in the world?
+      </h1>
       <DarkModeButton />
     </header>
   );
